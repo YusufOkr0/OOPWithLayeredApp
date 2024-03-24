b193fd84d82247295db137d5cfc133d25669b1f5
YusufOkr0 <Yusufokr0@gmail.com>
2024-03-24 11:38:44 +0300

    Initial commit

--- src/OOPWithNLayeredApp/Businnes/ProductManager.java
+++ src/OOPWithNLayeredApp/Businnes/ProductManager.java
@@ -1,16 +1,11 @@
 package OOPWithNLayeredApp.Businnes;
-import java.util.ArrayList;
-
 import OOPWithNLayeredApp.DataAccess.IDataAccess;
 import OOPWithNLayeredApp.Entities.Product;
-import OOPWithNLayeredApp.Logging.ILogger;
 
-public class ProductManager {
-
-	IDataAccess dao;
-	ArrayList<ILogger> Ilogger;
-	
-	public ProductManager(IDataAccess dao, ArrayList<ILogger> Ilogger) {
+public class ProductManager{
+IDataAccess dao;
+  	
+	public ProductManager(IDataAccess dao) {
 		this.dao = dao;
 	}
 	public void add(Product product) {
@@ -21,10 +16,6 @@ public class ProductManager {
 		
 		this.dao.delete(product);
 	}
-    
-    for(ILogger logger:this.Ilogger) {
-    	
-    	logger.log("14. SATIRDAKI HATA ");
-    }
-	
 }
+
+
--- src/OOPWithNLayeredApp/Main.java
+++ src/OOPWithNLayeredApp/Main.java
@@ -1,21 +1,16 @@
 package OOPWithNLayeredApp;
 
-import java.util.ArrayList;
-
 import OOPWithNLayeredApp.Businnes.ProductManager;
 import OOPWithNLayeredApp.DataAccess.JDBC;
 import OOPWithNLayeredApp.Entities.Product;
-import OOPWithNLayeredApp.Logging.CoreLog;
-import OOPWithNLayeredApp.Logging.EmailLog;
-import OOPWithNLayeredApp.Logging.ILogger;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
 		Product product = new Product("TV",1597);
-		ArrayList<ILogger> logger = {new EmailLog(),new CoreLog()};
-		ProductManager manager = new ProductManager(new JDBC(),logger);
+		
+		ProductManager manager = new ProductManager(new JDBC());
 		manager.add(product);
 	}
 }
